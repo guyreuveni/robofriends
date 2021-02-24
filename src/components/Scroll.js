80f34e061bcbbfaae5fd4b04f5f208566f6c6b9e
guy reuveni <guyreuveni98@gmail.com>
2021-02-24 22:39:59 +0200

    little changes

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', border: "5px solid black", height: '400px' }}>
+        <div style={{ overflowY: 'scroll', border: "5px solid black", height: '500px' }}>
             {props.children}
         </div>
     )
